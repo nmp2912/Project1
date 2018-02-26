5dd8c11c745b576b0ea5c237dfe31c30d2349d8e
nmp2912 <36055776+nmp2912@users.noreply.github.com>
2018-02-25 23:04:53 -0500

    Re-adding the Machine Class

--- dendron/src/dendron/machine/Machine.java
+++ dendron/src/dendron/machine/Machine.java
@@ -1,5 +1,3 @@
-package dendron.machine;
-
 import java.util.List;
 import java.util.Stack;
 import java.util.Map;
@@ -16,31 +14,20 @@ import dendron.Errors;
  * of nested class instantiation or to pass the symbol table and
  * stack into every instruction when it executes.)
  *
- * THIS CLASS IS INCOMPLETE. The student must add code to it.
  *
  * @author James Heliotis
- * @author YOUR NAME HERE
+ * @author Nicholas Pembroke
  */
 public class Machine {
 
+
+    ////////////////////////////////////////////////////////
+    ////////////     Private Methods/Fileds     ////////////
+    ////////////////////////////////////////////////////////
+
+
     /** Do not instatiate this class. */
     private Machine() {}
-
-    public static interface Instruction {
-        /**
-         * Run this instruction on the Machine, using the Machine's
-         * value stack and symbol table.
-         */
-        void execute();
-
-        /**
-         * Show the instruction using text so it can be understood
-         * by a person.
-         * @return a short string describing what this instruction will do
-         */
-        @Override
-        String toString();
-    }
 
     private static Map< String, Integer > table = null;
     private static Stack< Integer > stack = null;
@@ -54,30 +41,280 @@ public class Machine {
         table = new HashMap<>();
     }
 
+
+    ///////////////////////////////////////////////////
+    ////////////     Public Interfaces     ////////////
+    ///////////////////////////////////////////////////
+
+
     /**
-     * Generate a listing of a program on standard output by
-     * calling the toString() method on each instruction
-     * contained therein, in order.
-     *
-     * @param program the list of instructions in the program
+     * The instructions interface
      */
-    public static void displayInstructions(
-            List< Machine.Instruction > program ) {
-        System.out.println( "\nCompiled code:" );
-        for ( Machine.Instruction instr: program ) {
-            System.out.println( instr );
+    public interface Instruction {
+
+        /**
+         * Run this instruction on the Machine, using the Machine's value stack and symbol table
+         */
+        void execute();
+
+
+        /**
+         * Show the instruction using text so it can be understood by a person
+         *
+         * @return String - A short string describing what this instruction will do
+         */
+        @Override
+        String toString();
+
+    }
+
+
+    ////////////////////////////////////////////////
+    ////////////     Public Classes     ////////////
+    ////////////////////////////////////////////////
+
+
+    /**
+     * The ADD instruction
+     */
+    public static class Add implements Instruction{
+
+        //Run the microsteps for the ADD Instruction
+        @Override
+        public void execute() {
+            int op2 = stack.pop();
+            int op1 = stack.pop();
+            stack.push( op1 + op2 );
         }
-        System.out.println();
+
+        //Show the ADD instruction as plain text
+        //@return "ADD"
+        @Override
+        public String toString() {
+            return "ADD";
+        }
     }
 
     /**
-     * Run a "compiled" program by executing in order each instruction
-     * contained therein.
-     * Report on the final size of the stack (should normally be empty)
-     * and the contents of the symbol table.
-     * @param program a list of Machine instructions
+     * The DIVIDE instruction
      */
-    public static void execute( List< Instruction > program ) {
+    public static class Divide implements Instruction{
+
+        //Run the microsteps for the DIVIDE Instruction
+        @Override
+        public void execute() {
+            int op2 = stack.pop();
+            int op1 = stack.pop();
+            stack.push( op1 / op2 );
+        }
+
+        //Show the DIVIDE instruction as plain text
+        @Override
+        public String toString() {
+            return "DIVIDE";
+        }
+    }
+
+    /**
+     * The LOAD instruction
+     */
+    public static class Load implements Instruction{
+
+        //Run the microsteps for the LOAD Instruction
+        @Override
+        public void execute() {
+            String varname = table.get(null);
+            stack.push(varname);
+        }
+
+        //Show the LOAD instruction as plain text
+        @Override
+        public String toString() {
+
+            //TODO IDK if this is done
+            return "LOAD";
+        }
+    }
+
+    /**
+     * The MULTIPLY instruction
+     */
+    public static class Multiply implements Instruction{
+
+        //Run the microsteps for the MULTIPLY Instruction
+        @Override
+        public void execute() {
+            int op2 = stack.pop();
+            int op1 = stack.pop();
+            stack.push( op1 * op2 );
+        }
+
+        //Show the MULTIPLY instruction as plain text
+        @Override
+        public String toString() {
+            return "MULITPLY";
+        }
+    }
+
+    /**
+     * The NEGATE instruction
+     */
+    public static class Negate implements Instruction{
+
+        //Run the microsteps for the NEGATE Instruction
+        @Override
+        public void execute() {
+            int op1 = stack.pop();
+            stack.push( -1 * op1 );
+        }
+
+        //Show the NEGATE instruction as plain text
+        @Override
+        public String toString() {
+            return "NEGATE";
+        }
+    }
+
+    /**
+     * The PRINT instruction
+     */
+    public static class Print implements Instruction{
+
+        //Run the microsteps for the PRINT Instruction
+        @Override
+        public void execute() {
+            int op1 = stack.pop();
+            System.out.println("*** " + op1);
+        }
+
+        //Show the PRINT instruction as plain text
+        @Override
+        public String toString() {
+            return "PRINT";
+        }
+    }
+
+    /**
+     * The PUSHCONST instruction
+     */
+    public static class PushConst implements Instruction{
+
+        private int constant;
+
+        //Constructor
+        public PushConst(int constant) {
+            this.constant = constant
+        }
+
+        //Run the microsteps for the PUSHCONST Instruction
+        @Override
+        public void execute() {
+            stack.push( this.constant );
+        }
+
+        //Show the PUSHCONST instruction as plain text
+        @Override
+        public String toString() {
+            return "PUSHCONST";
+        }
+    }
+
+    /**
+     * The SQUAREROOT instruction
+     */
+    public static class SquareRoot implements Instruction{
+
+        //Run the microsteps for the SQUAREROOT Instruction
+        @Override
+        public void execute() {
+
+            int op1 = stack.pop();
+            stack.push( Math.sqrt(op1) );
+        }
+
+        //Show the SQUAREROOT instruction as plain text
+        @Override
+        public String toString() {
+            return "SQUAREROOT";
+        }
+    }
+
+    /**
+     * The STORE instruction
+     */
+    public static class Store implements Instruction{
+
+        //Store the name of the target variable
+        private String name;
+
+        //Constructor
+        public Store(String ident) {
+            this.name = ident;
+        }
+
+        //Run the microsteps for the STORE Instruction
+        @Override
+        public void execute() {
+            table.put( this.name, stack.pop() );
+        }
+
+        //Show the STORE instruction as plain text
+        //@return "STORE" followed by the target variable name
+        @Override
+        public String toString() {
+            return "STORE " + this.name;
+        }
+    }
+
+    /**
+     * the SUBTRACT instruction
+     */
+    public static class Subtract implements Instruction{
+
+        //Run the microsteps for the SUBTRACT Instruction
+        @Override
+        public void execute() {
+            int op2 = stack.pop();
+            int op1 = stack.pop();
+            stack.push( op1 - op2 );
+        }
+
+        //Show the SUBTRACT instruction as plain text
+        @Override
+        public String toString() {
+            return "SUBTRACT";
+        }
+    }
+
+
+    ////////////////////////////////////////////////
+    ////////////     Public Methods     ////////////
+    ////////////////////////////////////////////////
+
+
+    /**
+     *
+     * Generate a listing of a program on standard output by calling the
+     * toString() method on each instruction contained therein, in order
+     *
+     * @param program - a list of instructions in the program
+     *
+     */
+    public static void displayInstructions(List< Instruction > program) {
+            List< Machine.Instruction > program ) {
+            System.out.println( "\nCompiled code:" );
+            for ( Machine.Instruction instr: program ) {
+                System.out.println( instr );
+            }
+            System.out.println();
+    }
+
+    /**
+     * Run a "compiled" program by executing in order each instruction contained therein
+     *
+     * @param program - a list of Machine Instructions
+     */
+    public static void execute(List<Instruction> program) {
         reset();
         System.out.println("Executing compiled code...");
         for ( Instruction instr: program ) {
@@ -88,63 +325,4 @@ public class Machine {
         System.out.println();
         Errors.dump( table );
     }
-
-    /**
-     * The ADD instruction
-     */
-    public static class Add implements Instruction {
-        /**
-         * Run the microsteps for the ADD instruction.
-         */
-        @Override
-        public void execute() {
-            int op2 = stack.pop();
-            int op1 = stack.pop();
-            stack.push( op1 + op2 );
-        }
-
-        /**
-         * Show the ADD instruction as plain text.
-         * @return "ADD"
-         */
-        @Override
-        public String toString() {
-            return "ADD";
-        }
-    }
-
-    /**
-     * The STORE instruction
-     */
-    public static class Store implements Instruction {
-        /** stores name of target variable */
-        private String name;
-
-        /**
-         * Create a STORE instruction
-         * @param ident the name of the target variable
-         */
-        public Store( String ident ) {
-            this.name = ident;
-        }
-        /**
-         * Run the microsteps for the STORE instruction.
-         */
-        @Override
-        public void execute() {
-            table.put( this.name, stack.pop() );
-        }
-        /**
-         * Show the STORE instruction as plain text.
-         * @return "STORE" followed by the target variable name
-         */
-        @Override
-        public String toString() {
-            return "STORE " + this.name;
-        }
-    }
-
-    //
-    // ENTER YOUR CODE FOR THE OTHER INSTRUCTION CLASSES HERE.
-    //
 }
