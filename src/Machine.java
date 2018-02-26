2f2b1fb75ccc0b9d8abf26fd5123781d26740353
Nick Pembroke <nmp2912@rit.edu>
2018-02-25 21:02:55 -0500

    Finished properly stubbing out all methods

--- src/Machine.java
+++ src/Machine.java
@@ -11,26 +11,43 @@ public class Machine {
      */
     public static interface Instruction {
 
-        //Run this instruction on the Machine, using the Machine's value stack and symbol table
+        /**
+         * Run this instruction on the Machine, using the Machine's value stack and symbol table
+         */
         public void execute();
 
-        //Show the instruction using text so it can be understood by a person
+
+        /**
+         * Show the instruction using text so it can be understood by a person
+         *
+         * @return String - A short string describing what this instruction will do
+         */
         public String toString();
-
-    }
-
-    /**
-     * The LOAD instruction
-     */
-    private static class Load {
-
 
     }
 
     /**
      * The ADD instruction
      */
-    private static class Add {
+    public static class Add implements Instruction{
+
+        //Constructor
+        public Add() {
+
+        }
+
+        //Run the microsteps for the ADD Instruction
+        @Override
+        public void execute() {
+
+        }
+
+        //Show the ADD instruction as plain text
+        @Override
+        public String toString() {
+
+            return null;
+        }
 
 
     }
@@ -38,65 +55,199 @@ public class Machine {
     /**
      * The DIVIDE instruction
      */
-    private static class Divide {
+    public static class Divide implements Instruction{
 
+        //Constructor
+        public Divide() {}
 
+        //Run the microsteps for the DIVIDE Instruction
+        @Override
+        public void execute() {
+
+        }
+
+        //Show the DIVIDE instruction as plain text
+        @Override
+        public String toString() {
+            return null;
+        }
+    }
+
+    /**
+     * The LOAD instruction
+     */
+    public static class Load implements Instruction{
+
+        //Constructor
+        public Load(String ident) {
+
+        }
+
+        //Run the microsteps for the LOAD Instruction
+        @Override
+        public void execute() {
+
+        }
+
+        //Show the LOAD instruction as plain text
+        @Override
+        public String toString() {
+            return null;
+        }
     }
 
     /**
      * The MULTIPLY instruction
      */
-    private static class Multiply {
+    public static class Multiply implements Instruction{
 
+        //Constructor
+        public Multiply() {
+        }
 
+        //Run the microsteps for the MULTIPLY Instruction
+        @Override
+        public void execute() {
+
+        }
+
+        //Show the MULTIPLY instruction as plain text
+        @Override
+        public String toString() {
+            return null;
+        }
     }
 
     /**
      * The NEGATE instruction
      */
-    private static class Negate {
+    public static class Negate implements Instruction{
 
+        //Constructor
+        public Negate() {
+        }
 
+        //Run the microsteps for the NEGATE Instruction
+        @Override
+        public void execute() {
+
+        }
+
+        //Show the NEGATE instruction as plain text
+        @Override
+        public String toString() {
+            return null;
+        }
     }
 
     /**
      * The PRINT instruction
      */
-    private static class Print {
+    public static class Print implements Instruction{
 
+        //Constructor
+        public Print() {
+        }
 
+        //Run the microsteps for the PRINT Instruction
+        @Override
+        public void execute() {
+
+        }
+
+        //Show the PRINT instruction as plain text
+        @Override
+        public String toString() {
+            return null;
+        }
     }
 
     /**
      * The PUSHCONST instruction
      */
-    private static class PushConst {
+    public static class PushConst implements Instruction{
 
+        //Constructor
+        public PushConst(int constant) {
+        }
 
+        //Run the microsteps for the PUSHCONST Instruction
+        @Override
+        public void execute() {
+
+        }
+
+        //Show the PUSHCONST instruction as plain text
+        @Override
+        public String toString() {
+            return null;
+        }
     }
 
     /**
      * The SQUAREROOT instruction
      */
-    private static class SquareRoot {
+    public static class SquareRoot implements Instruction{
 
+        //Constructor
+        public SquareRoot() {
+        }
 
+        //Run the microsteps for the SQUAREROOT Instruction
+        @Override
+        public void execute() {
+
+        }
+
+        //Show the SQUAREROOT instruction as plain text
+        @Override
+        public String toString() {
+            return null;
+        }
     }
 
     /**
      * The STORE instruction
      */
-    private static class Store {
+    public static class Store implements Instruction{
 
+        //Constructor
+        public Store(String ident) {
+        }
 
+        //Run the microsteps for the STORE Instruction
+        @Override
+        public void execute() {
+
+        }
+
+        //Show the STORE instruction as plain text
+        @Override
+        public String toString() {
+            return null;
+        }
     }
 
     /**
      * the SUBTRACT instruction
      */
-    private static class Subtract {
+    public static class Subtract implements Instruction{
 
+        //Constructor
+        public Subtract() {
+        }
 
+        //Run the microsteps for the SUBTRACT Instruction
+        @Override
+        public void execute() {
+
+        }
+
+        //Show the SUBTRACT instruction as plain text
+        @Override
+        public String toString() {
+            return null;
+        }
     }
 
 
